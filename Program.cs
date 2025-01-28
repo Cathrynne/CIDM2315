3a82b113eb521a06f8df90b243f2e033ba289987
Cathrynne <catwotwo1@buffs.wtamu.edu>
2025-01-27 20:50:46 -0600

    Homework1

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@
             double X = 2.5;
             double Y = 3.3;
 
-            // Calculate Z based on the linear equation Z = 4*X*X + 3*Y
+            // Plug in Z based on the linear equation Z = 4*X*X + 3*Y
             double Z = 4 * X * X + 3 * Y;
 
-            // Print the result in the desired format
+            // Print the result in the correct format
             Console.WriteLine($"The value of Z when X = {X} and Y = {Y} is: {Z:F2}");
         }
     }
