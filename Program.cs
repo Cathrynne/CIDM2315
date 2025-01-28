8f8a6787845ce9e215ecb1572b9fb99678c9c8d6
Cathrynne <catwotwo1@buffs.wtamu.edu>
2025-01-27 20:48:20 -0600

    Upload Homework1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given values
+            double X = 2.5;
+            double Y = 3.3;
+
+            // Calculate Z based on the linear equation Z = 4*X*X + 3*Y
+            double Z = 4 * X * X + 3 * Y;
+
+            // Print the result in the desired format
+            Console.WriteLine($"The value of Z when X = {X} and Y = {Y} is: {Z:F2}");
+        }
+    }
+}
